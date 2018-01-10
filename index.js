dbfbebdb0b834fc52bbe558681b8c2a0e7af1b9e
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-10 00:51:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function dwarfRollCall(dwarves) {
+  var number = 0 
+  var string = ""
+  for (var i = 0; i<dwarves.length; i++) {
+    number = i + 1
+    string = string + number + ". " + dwarves[i] + " " 
+  }
+  return string
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for (var i= 0; i < planeteerCalls.length; i++) {
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!"
+  }
+  return planeteerCalls
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i ++) {
+    if (words.length > 4) {
+      return true
+    } else{
+      return false
+    }
+  }
 }
 
 function findTheCheese (foods) {
+  var cheese = ["cheddar", "gouda", "camembert"]
+  for (var i = 0; i < cheese.length; i++) {
+    for (var j = 0; j < foods.length; j++) {
+      if (cheese[i] == foods[j]) {
+        return foods[j]
+      }
+    }
+  }
+  return "no cheese!"
 }
