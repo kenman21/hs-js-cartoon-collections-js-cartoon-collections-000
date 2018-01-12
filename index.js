fe3bb78366e6aa70875e263d415b267b536f6d4e
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-12 21:02:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ function longPlaneteerCalls(words) {
 
 function findTheCheese (foods) {
   var cheese = ["cheddar", "gouda", "camembert"]
-  for (var i = 0; i < cheese.length; i++) {
-    for (var j = 0; j < foods.length; j++) {
-      if (cheese[i] == foods[j]) {
+  for (var i = 0; i < foods.length; i++) {
+    for (var j = 0; j < cheese.length; j++) {
+      if (foods[i] == cheese[j]) {
         return foods[j]
       }
     }
